bdac5fa9ba39402cdc1f54812702d982f6657aeb
R L <rl1987@sdf.lonestar.org>
2012-01-08 15:01:00 +0200

    Initial Commit

---
+++ Top Places/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Top Places
+//
+//  Created by avitela avitela on 2012-01-08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Top Places/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Top Places
+//
+//  Created by avitela avitela on 2012-01-08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
